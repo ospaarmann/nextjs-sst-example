10bff9414270308355fcb5e75f7b344d233f614d
Ole Spaarmann <os@codeshift.co>
2023-05-19 18:55:31 +0200

    Initial version with images and without SST

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "example-images-delude-prey-demerit.s3.eu-central-1.amazonaws.com",
+    ],
+    // These are the default values. We might have to adapt them
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
